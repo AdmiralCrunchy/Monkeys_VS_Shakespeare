ec7053d5907978a4854998b15f90f4952813ca49
Hoefer <ningboninja@gmail.com>
2022-08-03 13:40:24 -0700

    added user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require("bcrypt")
+const sequelize = require('../config/connection');
+
+
+class User extends Model { }
+
+User.init({
+    // add properites here, ex:
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8]
+        }
+    }
+}, {
+    sequelize,
+    hooks: {
+        beforeCreate: userObj => {
+            userObj.password = bcrypt.hashSync(userObj.password, 4);
+            return userObj;
+        }
+    }
+});
+
+module.exports = User
